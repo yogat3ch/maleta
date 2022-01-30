5c50931ec7b1bf592386813f3483c7e2122b79a2
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-30 16:50:33 -0500

    accessor functions

---
+++ R/accessor_fn_utils.R
@@ -0,0 +1,47 @@
+accessor_create <- function(.x) rlang::new_function(args =
+                      rlang::pairlist2(
+                        path = rlang::expr(!!.x),
+                        dep_update = dep_update_fn(fn = dep_update_dropbox),
+                        ... = ,
+                      ),
+                    body = base::quote({
+                      dep_update(path)
+                      UU::file_fn(path)(path, ...)
+                    }))
+
+do_assignment <- function(funs, ns = pkgload::pkg_name()) {
+  if (UU::is_legit(try(ns, silent = TRUE))) {
+    namespace <- rlang::ns_env(ns)
+    rlang::env_unlock(namespace)
+    purrr::iwalk(funs, ~{
+      if (exists(.y, envir = namespace, inherits = FALSE))
+        rlang::env_binding_unlock(namespace, .y)
+      assign(.y, .x, envir = namespace)
+      assignInNamespace(.y, .x, ns, envir = namespace)
+      rlang::env_binding_lock(namespace, .y)
+    })
+  } else
+    funs
+}
+
+#' Create accessor functions in the namespace
+#'
+#' @param dep_dir \code{(chr)} of the directory in which dependencies to be accessed with functions is stored. _Note:_ Either `dep_dir` or `deps` must be supplied.
+#' @param deps \code{(chr)} of full filepaths for all dependencies to be accessed with functions.
+#' @param dep_update \code{(function)} dependency update from remote location function
+#'
+#' @return The accessor functions will be available in the package namespace if a `golem` package, otherwise accessor functions will be returned in a list
+#' @export
+
+
+create_accessors <- function(dep_dir = "data", deps = NULL, dep_update = dep_update_fn(fn = dep_update_dropbox)) {
+  if (is.null(deps))
+    deps <- clean_null(UU::list.files2(dep_dir)) |>
+      stringr::str_subset("\\.png^", negate = TRUE)
+
+  UU::mkpath(dep_dir)
+  purrr::walk(deps, dep_update)
+  accessor_funs <- purrr::map(UU::list.files2(path), accessor_create)
+  do_assignment(accessor_funs)
+}
+
